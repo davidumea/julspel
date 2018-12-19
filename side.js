b331a7a15295fb8c159cee134906f7bc5e2f49c4
anda16 <te16.david.andersson@umea.nti.se>
2018-12-19 11:15:37 +0100

    fixade collision, paktet respawnar vid collision och den räknar score, har fixat men inte implementerat några bilder

--- side.js
+++ side.js
@@ -1,6 +1,12 @@
 let canvas = document.getElementById("spel");
 let ctx = canvas.getContext("2d");
 let fgImg = document.getElementById("fg-gfx");
+let eldImg = document.getElementById("eld-gfx");
+let px;
+let py;
+
+lives = 3;
+score = 0;
 
 playerSpawnPointX = 440;
 playerSpawnPointY = 700;
@@ -17,7 +23,7 @@ function newPlayer(x, y) {
 	let player = {
 		x: x,
 		y: y,
-		speed: 6,
+		speed: 9,
 		size: 20
 	}
 	return player;
@@ -35,10 +41,40 @@ function newPresent(x, y) {
 
 function drawPlayer(pObject) {
 	ctx.drawImage(fgImg, pObject.x, pObject.y, pObject.size, pObject.size);
+	if (pObject.x <= 0) {
+		pObject.x = 879;
+	} else if (pObject.x >= 880) {
+		pObject.x = 1;
+	}
+	if (lives == 0) {
+		players.splice(players.indexOf(pObject),1);
+	}
+	px = pObject.x;
+	py = pObject.y;
+	return px, py;
 }
 
 function drawPresent(prObject, x, y, size) {
 	ctx.drawImage(fgImg, prObject.x, prObject.y, prObject.size, prObject.size);
+	if (prObject.y > 800) {
+		presents.splice(presents.indexOf(prObject),1);
+		lives--;
+		console.log("Live(s): " + lives);
+		if (lives == 0) {
+			presents.splice(presents.indexOf(prObject),1);
+			console.log("du är trash");
+		} else {
+			presents.push(newPresent(Math.floor((Math.random()*879) + 1), presentSpawnPointY));
+		}
+	}
+	if (prObject.x + 20 >= px && prObject.x <= px + 20 && prObject.y + 20 >= py) {
+		presents.splice(presents.indexOf(prObject),1);
+		score++;
+		console.log("Score: " + score);
+		if (lives > 0) {
+			presents.push(newPresent(Math.floor((Math.random()*879) + 1), presentSpawnPointY));
+		}
+	}
 }
 
 let move = {
@@ -69,6 +105,8 @@ document.addEventListener("keyup", function(e) {
 })
 
 function draw() {
+	ctx.clearRect(0,0,900,900);
+	ctx.drawImage(eldImg, 0, 800, 200, 900);
 
 	ctx.clearRect(0, 0, 900, 900);
 	players.forEach(element => {
@@ -86,23 +124,18 @@ function draw() {
 		drawPresent(element);
 		if (element.y >= 10) {
 			element.speed = 2;
-		} if (element.y >= 30) {
+		} if (element.y >= 20) {
 			element.speed = 3;
-		} if (element.y >= 60) {
+		} if (element.y >= 40) {
 			element.speed = 4;
-		} if (element.y >= 100) {
+		} if (element.y >= 70) {
 			element.speed = 5;
-		} if (element.y >= 150) {
+		} if (element.y >= 110) {
 			element.speed = 6;
-		} if (element.y >= 210) {
+		} if (element.y >= 160) {
 			element.speed = 7;
-		} if (element.y >= 280) {
+		} if (element.y >= 210) {
 			element.speed = 8;
-		}
-		if (element.y >= 800) {
-			ctx.clearRect(element.x, element.y, element.size, element.size);
-			newPresent(presentSpawnPointX, presentSpawnPointY);
-			drawPresent(fgImg, element.x, element.y, element.size, element.size);
 		}
 	});
 	window.requestAnimationFrame(draw);
